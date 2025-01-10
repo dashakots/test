154e555dd061f4f1456c53a93968cc1c4a600cbe
dashakots <dkatsuba.work@gmai.com>
2025-01-10 21:14:29 +0300

    js

---
+++ lesson11/index.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// let a = 1000;
+// console.log(a);
+// a = 500;
+// console.log(a);
+
+// const b = 1000;
+// console.log(b);
+
+// const c1 = 43;
+// console.log(c1);
+
+// console.log(65456451 / 0);
+
+// const str = `1+1=${1 + 1}`;
+// console.log(str);
+// const myName = "Darya";
+// console.log(`my name is ${myName}`);
+// alert("message");
+
+// confirm("please enter");
+
+// const nickName = prompt("please write your name", "darya");
+// console.log(`Hello, ${nickName}`);
+
+// const a = Symbol("id");
+// const b = Symbol("id");
+// console.log(a.description);
+// const obj =  {
+// firstName: "John",
+// lastName: "Smith",
+// age: 29,
+// edu: {
+//   secondary:{
+//     name:"fdgfgf",
+//     year: "2015",
+//   },
+//     secondary:{
+//     name:"fdgfgf",
+//     year: "2015",
+//   }
+// }
+// }
+
+const num = 46;
+console.log(num, typeof num);
+
+// const numStr1 = Number(str);
+// console.log(numStr1, typeof numStr1);
+
+// const numStr2 = +str;
+// console.log(numStr2, typeof numStr2);
+
+// const numStr3 = parseInt(str);
+// console.log(numStr3, typeof numStr3);
+
+const strNum1 = String(num);
+console.log(strNum1, typeof strNum1);
+
+const strNum3 = num.toString();
+console.log(strNum3, typeof strNum3);
+const strNum2 = num + "";
+console.log(strNum2, typeof strNum2);
