b69788cc67982da36e98bdc1370433f71cc73eec
dashakots <dkatsuba.work@gmai.com>
2025-01-14 21:19:43 +0300

    lesson12

---
+++ lesson12/index.js
@@ -0,0 +1,34 @@
+"use strict!";
+
+let str = "SOME random STRing";
+let str2 = "next string";
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+// const strUp = str.toUpperCase();
+// console.log(strUp);
+
+// console.log(str.concat(str2, "dfjhbgdfjhbg", 3654));
+// console.log("50" + 25); получается строка
+// console.log("50" - 25); получается число
+
+// console.log(str.repeat(5));
+
+// console.log(str.toLowerCase().includes("str"));
+
+// console.log(str.startsWith("SOME"));
+// console.log(str.endsWith("STRing!"));
+
+// console.log(str.replace(/s/gi, "$"));
+
+// console.log(str.charAt(6));
+
+// console.log(str.indexOf("random"));
+// console.log(str.lastIndexOf("S"));
+
+// console.log(str.slice(5, 11));
+// console.log(str.substring(5, 11));
+// console.log(str.substr(5, 6));
+
+// console.log(str.trim());
---
+++ lesson12/index2.js
@@ -0,0 +1,79 @@
+"use strict";
+
+// const arr = [1, "two", null, true, { a: 2 }, [1, 2, 3]];
+// const obj = arr[4];
+// console.log(obj);
+
+// console.log(arr[5][1]);
+
+// const arr2 = arr[5];
+// console.log(arr2[1]);
+
+const arr = [0, 1, 2, 3, 4];
+const arr2 = [5, 6, 7, 8, 9];
+
+// console.log(arr.concat(arr2, 11, "cfh"));
+
+// const elemsCount = arr.push("five", "six", "seven", "eight");
+// console.log(arr);
+// console.log(elemsCount);
+
+// const elemsCount = arr.unshift("five", "six", "seven", "eight");
+// console.log(arr);
+// console.log(elemsCount);
+
+// const data = arr.pop();
+// console.log(arr);
+// console.log(data);
+
+// const data = arr.shift();
+// console.log(arr);
+// console.log(data);
+// arr.reverse();
+// console.log(arr.reverse());
+
+// const revArr = arr.toReversed();
+// console.log(arr);
+// console.log(revArr);
+
+// console.log("arr", arr);
+
+// const arrSlice = arr.slice(1, 4);
+// console.log("slice", arrSlice);
+// console.log("arr", arr);
+
+// const arrSplice = arr.splice(1, 0, 11, 22, 33);
+// console.log("splice", arrSlice);
+// console.log("arr", arr);
+
+// const age = 18;
+
+// if (age > 18) {
+// 	console.log("> 18");
+// } else if (age < 18) {
+// 	console.log("< 18");
+// } else {
+// 	console.log("= 18");
+// }
+
+// if (age < 18) {
+// 	console.log("<18");
+// } else if (age < 19) {
+// 	console.log("< 19");
+// } else if (age < 20) {
+// 	console.log("< 20");
+// } else if (age < 21) {
+// 	console.log("< 21");
+// }
+
+// console.log(1 == "1");
+// console.log(1 === "1");
+
+// console.log(1 != "1");
+// console.log(1 !== "1");
+
+// const name = "John";
+
+// if (name !== "John") {
+// 	console.log("welcome");
+// }
